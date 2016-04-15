1a26573be6c5bcb469bb95c16c60a1ab36c4eb58
sOUTHeR <southeryf@live.com>
2016-04-15 18:02:41 +0800

    add Project;

---
+++ AppxDeployTool/MainForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppxDeployTool
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ChooseFileBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
